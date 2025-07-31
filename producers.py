9bbc9987dbeb7ae6d914ccb21caa105e788942ed
Stefania Manea <110382617+stefy-d@users.noreply.github.com>
2025-07-31 11:00:36 +0300

    update prod

--- producers.py
+++ producers.py
@@ -4,41 +4,28 @@ import time
 import threading
 from confluent_kafka import Producer
 from confluent_kafka.admin import AdminClient
-
+from more_itertools import chunked
 
 bootstrap_servers = 'localhost:9092'
 
 
 def stream_file(path, topic):
-    batch_size=50
+    from confluent_kafka import Producer
+    batch_size = 50
     print(f"[{topic}] Starting stream from {path}")
     producer = Producer({'bootstrap.servers': bootstrap_servers})
 
-    # TODO: sa vad daca pot sa fac altfel batch urile
-    # TODO: batchuri mai mici ca sa nu se termine streamingul asa repede
-    batch = []
     with open(path, 'r') as f:
-        for line in f:
-            line = line.strip()
-            if line:
-                batch.append(line)
-
-            # trimit pe topicul corespunzator
-            if len(batch) >= batch_size:
-                message = "\n".join(batch)
+        for chunk in chunked(f, batch_size):
+            lines = [line.strip() for line in chunk if line.strip()]
+            if lines:
+                message = "\n".join(lines)
                 producer.produce(topic=topic, value=message.encode('utf-8'))
                 producer.poll(0)
-                batch = []
-                time.sleep(0.1) # delay pt a simula flux de date
-
-    if batch:
-        message = "\n".join(batch)
-        producer.produce(topic=topic, value=message.encode('utf-8'))
-        producer.poll(0)
+                time.sleep(0.1)
 
     producer.flush()
     print(f"[{topic}] Finished streaming.")
-
 
 
 def run_producers(paths, topics):
